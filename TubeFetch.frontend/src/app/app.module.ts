1847cc4d5ec01ffe01b6da0aee00f71c949591e7
Andrea Girlando <andreagirlando05@gmail.com>
2025-02-21 22:38:57 +0100

    aggiunta dello spinner

--- TubeFetch.frontend/src/app/app.module.ts
+++ TubeFetch.frontend/src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import { importProvidersFrom, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
+import {NgxSpinnerModule} from 'ngx-spinner'
+import { loadingInterceptor } from './services/loading.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,12 +19,16 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent,
   ],
   imports: [
+    NgxSpinnerModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptors([loadingInterceptor])),
+    importProvidersFrom([BrowserAnimationsModule])
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
