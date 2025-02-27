67c199ea37798514d6748ee2f0c45c819aca5c6a
Andrea Girlando <andreagirlando05@gmail.com>
2025-02-27 15:11:40 +0100

    Eliminazione file inutili

--- TubeFetch.api/test.py
+++
@@ -1,4 +0,0 @@
-from externalApi.youtube import fromYoutubePlaylistGetLinks
-
-
-print(fromYoutubePlaylistGetLinks("https://www.youtube.com/playlist?list=PL4fGSI1pDJn6puJdseH2Rt9sMvt9E2M4i").toJson())
